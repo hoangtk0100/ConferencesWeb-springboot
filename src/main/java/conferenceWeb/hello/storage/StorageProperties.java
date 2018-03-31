6b793e68e98c27d0cfc8a661cdc0ca9f4ab5356f
sharingan0100 <sharingan.0100@gmail.com>
2018-03-31 15:59:50 +0700

    hihi

---
+++ src/main/java/conferenceWeb/hello/storage/StorageProperties.java
@@ -0,0 +1,23 @@
+package conferenceWeb.hello.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Repository;
+
+@ConfigurationProperties("storage")
+@Repository
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
